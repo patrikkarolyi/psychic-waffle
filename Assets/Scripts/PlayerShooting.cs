de0d2addbe99986a2f377ff3eec9a6b60f806fad
Patrik Károlyi <patrikkarolyi@hotmail.com>
2020-02-25 22:54:46 +0100

    fixed animations and added health slider

---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public int m_PlayerNumber = 1; // Used to identify the different players.
+    public Rigidbody m_Bullet; // Prefab of the shell.
+    public Transform m_FireTransform; // A child of the tank where the shells are spawned.
+
+    private string m_FireButton; // The input axis that is used for launching shells.
+    private float m_CurrentLaunchForce; // The force that will be given to the shell when the fire button is released.
+
+
+    private void Start()
+    {
+        m_FireButton = "Fire" + m_PlayerNumber;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown(m_FireButton))
+        {
+            Fire();
+        }
+    }
+
+
+    private void Fire()
+    {
+        Rigidbody bulletInstance = Instantiate(m_Bullet, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+
+        bulletInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward;
+        ;
+    }
+}
